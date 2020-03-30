b209fac66701197bc556200b14ca0c80fe6b2fde
Adrian Sims <adriansims62781@gmail.com>
2020-03-30 21:05:15 +0200

    Updates.

--- SrtMod/SrtFile.cs
+++ SrtMod/SrtFile.cs
@@ -18,15 +18,16 @@ namespace SrtMod
             public int Serial { get; set; }
             public int MilliSecondsStart { get; set; }
             public int MilliSecondsEnd { get; set; }
-            public List<string> Text { get; set; }
+            public List<string> Text { get; set; } = new List<string>();
         }
 
         private State _state = State.Init;
-        private const string timePattern = @"^\s+\d\d:\d\d:\d\d,\d\d\d\s+-->\s+\d\d:\d\d:\d\d,\d\d\d\s+$";
+        private const string timePattern = @"^\s*\d\d:\d\d:\d\d,\d\d\d\s*-->\s*\d\d:\d\d:\d\d,\d\d\d\s*$";
         private List<string> _lineErrors = new List<string>();
 
         private string _filename;
 
+        private List<SrtEntry> _srtEntries = new List<SrtEntry>();
         
         public SrtFile(string filename)
         {
@@ -72,6 +73,7 @@ namespace SrtMod
                     if (string.IsNullOrEmpty(line))
                     {
                         _state = State.Init;
+                        _srtEntries.Add(currentSrtEntry);
                     }
                     else
                     {
@@ -81,6 +83,38 @@ namespace SrtMod
             }
         }
 
+        public void AddWholeFile(int milliseconds)
+        {
+            foreach (var entry in _srtEntries)
+            {
+                entry.MilliSecondsStart += milliseconds;
+                entry.MilliSecondsEnd += milliseconds;
+            }
+        }
+
+        public void Save(string outputFilename)
+        {
+            using (var writer = new StreamWriter(outputFilename, true))
+            {
+                foreach (var entry in _srtEntries)
+                {
+                    writer.WriteLine($"{entry}");
+                    GetStringFromMilliseconds(entry.MilliSecondsStart, entry.MilliSecondsEnd, out var str);
+                    writer.WriteLine($"{str}");
+                    foreach (var textline in entry.Text)
+                    {
+                        writer.WriteLine($"{textline}");
+                    }
+                    writer.WriteLine();
+                }
+            }
+        }
+
+        private void GetStringFromMilliseconds(int milliSecondsStart, int milliSecondsEnd, out string str)
+        {
+            throw new NotImplementedException();
+        }
+
         private void GetMilliSeconds(string line, out int start, out int end)
         {
             var h = 10 * (line[0] - '0') + line[1] - '0';
@@ -88,7 +122,7 @@ namespace SrtMod
             var s = 10 * (line[6] - '0') + line[7] - '0';
             var ms = 100 * (line[9] - '0') + 10 * (line[10] - '0') + line[11] - '0';
 
-            start = h * 3_600_000 + m * 60_000 * s * 1000 + ms;
+            start = h * 3_600_000 + m * 60_000 + s * 1000 + ms;
 
             var offset = line.IndexOf('>');
             while (!(char.IsDigit(line[offset])))
@@ -101,7 +135,7 @@ namespace SrtMod
             s = 10 * (line[offset + 6] - '0') + line[offset + 7] - '0';
             ms = 100 * (line[offset + 9] - '0') + 10 * (line[offset + 10] - '0') + line[offset + 11] - '0';
 
-            end = h * 3_600_000 + m * 60_000 * s * 1000 + ms;
+            end = h * 3_600_000 + m * 60_000 + s * 1000 + ms;
         }
     }
 }
